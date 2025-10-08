52672fd68bc736360218fcb9af7da0a890ed0cfe
bamgyu07 <alyamafaza44@gmail.com>
2025-10-08 09:35:56 +0700

    Add CetakKRS03 class for UKT payment verification

---
+++ Jobsheet05/CetakKRS03.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CetakKRS03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverivikasi");
+            System.out.println("Silahkan Cetak KRS dan minta tanda tanggan DPA");
+        } 
+        else {
+            System.out.println("Pembayaran UKT belum terverivikasi");
+            System.out.println("Silahkan lakukan pembayaran UKT terlebih dahulu");
+        }
+     sc.close();
+    }
+}
