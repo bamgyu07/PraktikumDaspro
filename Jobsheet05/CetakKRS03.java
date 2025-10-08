e0fb0e704fc8e07fc0727096f82de6d590295158
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-10-08 10:31:26 +0700

    Percobaan 1

---
+++ Jobsheet05/CetakKRS03.java
@@ -0,0 +1,19 @@
+package Jobsheet05;
+
+import java.util.Scanner;
+
+public class CetakKRS03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverivikasi");
+            System.out.println("Silahkan Cetak KRS dan minta tanda tanggan DPA");
+        }
+    sc.close();
+    }
+}
