253cd548f0beb91d409aba7aa6496871f2a24947
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-10-08 08:34:49 +0700

    Jobsheet05

---
+++ Jobsheet05/CetakKRS03.java
@@ -0,0 +1,16 @@
+import java.until.Scanner;
+
+public class CetakKRS03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("---Cetak KRS SIAKAD ---")
+        System.out.println("Apakah UKT sudah lunas? (true/false): boolean uktLunas =sc.nextBoolean();");
+
+        if (uktLunas) {
+            System.out.print("Pembayaran UKT terverifikasi");
+            System.out.print("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+
+        }
+    }
