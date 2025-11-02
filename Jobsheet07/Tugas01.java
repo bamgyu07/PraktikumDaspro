b103af513d68191ed87a83e4476806f0cd48f01c
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-11-02 07:02:29 +0700

    Fix ticket discount logic and input validation

--- Jobsheet07/Tugas01.java
+++ Jobsheet07/Tugas01.java
@@ -17,23 +17,24 @@ public class Tugas01 {
 
         int i= 0;
 
-        while (i < jumlahTiket) {
-            if (jumlahTiket > 4) {
-            totalBayar = totalHarga - (totalHarga * diskon1);
-            System.out.println("Anda mendapatkan diskon 10%.");
-            System.out.println("Total yang harus dibayar: Rp" + totalBayar);
-            break;
-        } else if (jumlahTiket > 10) {
-            totalBayar = totalHarga - (totalHarga * diskon2);
-            System.out.println("Anda mendapatkan diskon 15%.");
-            System.out.println("Total yang harus dibayar: Rp" + totalBayar);
-            break;  
-        } else {
-            totalBayar = totalHarga;
-            System.out.println("Tidak ada diskon.");
-            System.out.println("Total yang harus dibayar: Rp" + totalBayar);
-        }
+       while (jumlahTiket <= 0) {
+            System.out.println("Jumlah tiket tidak valid, silakan masukkan lagi.");
+            return;
+            } if (jumlahTiket > 10) {
+                totalBayar = totalHarga - (totalHarga * diskon2);
+                System.out.println("Anda mendapatkan diskon 15%.");
+                System.out.println("Total yang harus dibayar: Rp" + totalBayar);
+            } else if (jumlahTiket > 4) {
+                totalBayar = totalHarga - (totalHarga * diskon1);
+                System.out.println("Anda mendapatkan diskon 10%.");
+                System.out.println("Total yang harus dibayar: Rp" + totalBayar);
+            } else {
+                totalBayar = totalHarga;
+                System.out.println("Tidak ada diskon.");
+                System.out.println("Total yang harus dibayar: Rp" + totalBayar);
+            }
             i++;
         }
     }
 }
+
