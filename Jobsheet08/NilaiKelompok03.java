a5e3d10db01f9deb69473d06d98914b00af6c2df
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-11-08 17:03:33 +0700

    nilai rata-rata kelompok tertingi

--- Jobsheet08/NilaiKelompok03.java
+++ Jobsheet08/NilaiKelompok03.java
@@ -6,23 +6,33 @@ public class NilaiKelompok03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, totalNilai, rataNilai;
+        double nilai, totalNilai, rataNilai, maxRata = 0;
+        int KelompokTerbaik = 0;
         totalNilai = 0;
 
-        int i = 1;
-        while (i <= 6) {
-            System.out.println("kolompok " + i );
-            totalNilai = 0;
-
-            for (int j = 1; j <= 5; j++) {
-                System.out.print("Masukkan nilai ke- " + j + ": ");
-                nilai = sc.nextDouble();
-                totalNilai += nilai;
+            int i = 1;
+            while (i <= 6) {
+                System.out.println("kelompok " + i );
+                totalNilai = 0;
+    
+                for (int j = 1; j <= 5; j++) {
+                    System.out.print("Masukkan nilai ke- " + j + ": ");
+                    nilai = sc.nextDouble();
+                    totalNilai += nilai;
+                }
+                rataNilai = totalNilai / 5;
+                System.out.println("Total nilai: " + totalNilai);
+                System.out.println("Rata-rata: " + rataNilai);
+    
+                if (rataNilai > maxRata) {
+                    maxRata = rataNilai;
+                    KelompokTerbaik = i;
+                }
+    
+                i++;
             }
-            rataNilai = totalNilai / 5;
-            System.out.println("Total nilai: " + totalNilai);
-            i++;
+    
+            System.out.println("Kelompok dengan rata-rata tertinggi adalah kelompok " + KelompokTerbaik + " dengan nilai rata-rata " + maxRata);
+            sc.close();
         }
-        
     }
-}
