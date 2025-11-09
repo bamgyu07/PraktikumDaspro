3395da504f0238cbc69643a8b94c36bce8631e92
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-11-09 09:18:27 +0700

    buat kotak angka 5

--- Jobsheet08/Tugas2.java
+++ Jobsheet08/Tugas2.java
@@ -1,5 +1,22 @@
 package Jobsheet08;
 
+import java.util.Scanner;
+
 public class Tugas2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("5 ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
