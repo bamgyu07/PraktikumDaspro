894bd8e1b9d1612d4170c6bd651b6a150996e8b4
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-11-12 10:53:55 +0700

    array bilangan

---
+++ Jobsheet09/ArrayNilai03.java
@@ -0,0 +1,18 @@
+package Jobsheet09;
+
+import java.util.Scanner;
+
+public class ArrayNilai03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + (i + 1) + ": ");
+            nilaiAkhir[i] = sc.nextInt();
+        } for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" + (i + 1) + ": " + nilaiAkhir[i]);
+        }
+    }
+}
