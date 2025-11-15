09a894d5afada22f58e6d1ed5e085a8c99875356
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-11-15 15:26:11 +0700

    modif

--- Jobsheet09/SearchNilai03.java
+++ Jobsheet09/SearchNilai03.java
@@ -1,19 +1,37 @@
 package Jobsheet09;
 
+import java.util.Scanner;
+
 public class SearchNilai03 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int inputNilai = sc.nextInt();
+        
+        int[] arrNilai = new int [inputNilai];
+        int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+            System.out.print("Masukkan nilai ke-" + (i+1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int nilai = sc.nextInt();
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (nilai == arrNilai[i]) {
                 hasil = i;
                 break;
-            }
+            } 
         }
         System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        if (hasil != -1) {
+            System.out.println("Nilai " + nilai + " ketemu, merupakan nilai mahasiswa ke-" + (hasil+1));
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
         System.out.println();
     }
 }
