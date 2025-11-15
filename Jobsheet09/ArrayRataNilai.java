99bd0aaf1e2e66386e8750970954da7695cbd0f7
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-11-15 14:55:25 +0700

    Mahasiswa yang tidak lulus

--- Jobsheet09/ArrayRataNilai.java
+++ Jobsheet09/ArrayRataNilai.java
@@ -6,17 +6,34 @@ public class ArrayRataNilai {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
+
         double total = 0;
         double rata2;
+        int[] nilaiMhs = new int [jmlMhs];
+        int tidakLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
             nilaiMhs[i] = sc.nextInt();
-        } for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
         }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+            if (nilaiMhs[i] < 70) {
+                tidakLulus++;
+            }
+        }
+
         rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai mahasiswa: " + rata2);
+        System.out.println("Rata-rata nilai: " + rata2);
+
+        if (rata2 > 70) {
+            System.out.println("Rata-rata nilai lulus");
+        } else {
+            System.out.println("Rata-rata nilai tidak lulus");
+        }
+
+        System.out.println("Banyak mahasiswa yang tidak lulus: " + tidakLulus);
     }
 }
