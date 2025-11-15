898b2d45121ec2db7175341c1b9a1dc67ae969bc
Aliyah Mafazah <alyamafaza7@gmail.com>
2025-11-15 11:24:35 +0700

    modif statement

--- Jobsheet09/ArrayNilai03.java
+++ Jobsheet09/ArrayNilai03.java
@@ -8,11 +8,15 @@ public class ArrayNilai03 {
 
         int[] nilaiAkhir = new int[10];
         
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + (i + 1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
-        } for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + (i + 1) + ": " + nilaiAkhir[i]);
+        } for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } if (nilaiAkhir[i] < 70) {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+            }
         }
     }
 }
